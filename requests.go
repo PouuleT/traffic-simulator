e691f211bc8d610852348884f6fbb0d638fadeba
Lucas BEE <pouulet@gmail.com>
2016-07-13 11:15:44 +0200

    Massive rewrite (#12)
    An object TrafficGenerator spawns Workers that make requests depending
    on the type given in argument ( http / dns )
    Workers generates Requests which are now interfaces
    - A Request knows how to print itself, return its status, its size, its
      duration and implement the Error() interface
      Requests are added to a Stats object, which is also an interface
    - A Stats obj knows how to AddRequests, SetDuration of the
      execution and to Render itself at the end of the program

--- requests.go
+++ requests.go
@@ -1,40 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"math/rand"
-	"net"
-	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/fatih/color"
 )
 
+const (
+	// Success is a successful request
+	Success criticityLevel = iota
+	// Warning is a successful request but with a bad status
+	Warning
+	// Critical is an unsuccessful request
+	Critical
+)
+
+// criticityLevel represents the criticity level of a request
+type criticityLevel int
+
 // Colors
 var red = color.New(color.FgRed).SprintfFunc()
 var green = color.New(color.FgGreen).SprintfFunc()
 var yellow = color.New(color.FgYellow).SprintfFunc()
-
-// Request represents a requsest reponse, with the return code and the duration
-type Request struct {
-	status      string
-	statusShort string
-	url         string
-	criticity   criticityLevel
-	duration    time.Duration
-	err         error
-}
-
-type criticityLevel int
-
-const (
-	Success  criticityLevel = iota
-	Warning  criticityLevel = iota
-	Critical criticityLevel = iota
-)
 
 var criticityColor = map[criticityLevel]func(string, ...interface{}) string{
 	Success:  green,
@@ -42,91 +29,12 @@ var criticityColor = map[criticityLevel]func(string, ...interface{}) string{
 	Critical: red,
 }
 
-// findRandomURL will return a random URL
-func findRandomURL() string {
-	return URLs[rand.Intn(len(URLs))]
-}
-
-// getURL will get a given URL and return a *Request
-func getURL(url string) *Request {
-	client := http.DefaultClient
-	client.Timeout = time.Duration(timeout) * time.Second
-	url = "http://" + url
-
-	var dur time.Duration
-	t := time.Now()
-	resp, err := client.Get(url)
-	if err != nil {
-		dur = time.Since(t)
-		return &Request{
-			url:       url,
-			duration:  dur,
-			err:       err,
-			criticity: Critical,
-		}
-	}
-	defer resp.Body.Close()
-
-	// Read the full body
-	io.Copy(ioutil.Discard, resp.Body)
-	// Record the duration of the request
-	dur = time.Since(t)
-
-	// If some http statuses has no StatusText, return a simple string with the
-	// http status
-	statusText := http.StatusText(resp.StatusCode)
-	if statusText == "" {
-		statusText = fmt.Sprintf("%d", resp.StatusCode)
-	}
-
-	var reqCriticity criticityLevel
-	if resp.StatusCode == http.StatusOK {
-		reqCriticity = Success
-	} else {
-		reqCriticity = Warning
-	}
-
-	return &Request{
-		url:         url,
-		duration:    dur,
-		status:      statusText,
-		statusShort: strconv.Itoa(resp.StatusCode),
-		criticity:   reqCriticity,
-	}
-}
-
-// lookupURL will make a DNS request on a given URL and return a *Request
-func lookupURL(url string) *Request {
-	var dur time.Duration
-	t := time.Now()
-	// Make the DNS request
-	_, err := net.LookupHost(url)
-	if err != nil {
-		dur = time.Since(t)
-		return &Request{
-			duration:  dur,
-			err:       err,
-			criticity: Critical,
-		}
-	}
-
-	// Record the duration of the request
-	dur = time.Since(t)
-
-	return &Request{
-		duration:  dur,
-		status:    "OK ",
-		criticity: Success,
-		url:       url,
-	}
-}
-
-// Log will return the string to display on a log
-func (r *Request) Log() string {
-	// If there is a short status, print it instead of a long one
-	statusToShow := r.statusShort
-	if statusToShow == "" {
-		statusToShow = r.status
-	}
-	return fmt.Sprintf("| %s | %12s | Get %s", criticityColor[r.criticity](statusToShow), r.duration, r.url)
+// Request represents a Request interface
+type Request interface {
+	String() string
+	Error() string
+	IsError() bool
+	Status() string
+	Size() int64
+	Duration() time.Duration
 }
